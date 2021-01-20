2028a477d8d5f5d880c4d3041ac770225f324a04
Ajay <manigandann.b@gmail.com>
2021-01-20 16:35:18 +0530

    calculator app

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.scss']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  input:string = '';
+  result:string = '';
+  
+ 
+  pressNum(num: string) {
+    
+    this.input = this.input + num
+    this.calcAnswer();
+  }
+ 
+ 
+  getLastOperand() {
+    let pos:number;
+    console.log(this.input)
+    pos=this.input.toString().lastIndexOf("+")
+    if (this.input.toString().lastIndexOf("-") > pos) pos=this.input.lastIndexOf("-")
+    if (this.input.toString().lastIndexOf("*") > pos) pos=this.input.lastIndexOf("*")
+    if (this.input.toString().lastIndexOf("/") > pos) pos=this.input.lastIndexOf("/")
+    console.log('Last '+this.input.substr(pos+1))
+    return this.input.substr(pos+1)
+  }
+ 
+ 
+  pressOperator(op: string) {
+ 
+    //Do not allow operators more than once
+    const lastKey = this.input[this.input.length - 1];
+    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+')  {
+      return;
+    }
+   
+    this.input = this.input + op
+    this.calcAnswer();
+  }
+ 
+ 
+  clear() {
+    if (this.input !="" ) {
+      this.input=this.input.substr(0, this.input.length-1)
+    }
+  }
+ 
+  allClear() {
+    this.result = '';
+    this.input = '';
+  }
+ 
+  calcAnswer() {
+    let formula = this.input;
+    let lastKey = formula[formula.length - 1];
+    if (lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
+    }
+ 
+    lastKey = formula[formula.length - 1];
+ 
+    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+' || lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
+    }
+    this.result = eval(formula);
+    if(this.result=='Infinity'){
+      this.result='Math Error'
+    }
+  }
+ 
+  getAnswer() {
+    this.calcAnswer();
+    this.input = this.result;
+    if (this.input=="0") this.input="";
+  }
+ 
+
+}
